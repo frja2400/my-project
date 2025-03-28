74bf1f862fc383339ca198408af5b9ba6f097bfb
Frida Jansson <frja2400@student.miun.se>
2025-03-28 09:49:54 +0100

    Började TS-koda så att man kan addera kurser och manipulera DOM.

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,35 @@
 import './style.css';
+
+interface courseInfo {
+    code: string,
+    name: string,
+    progression: 'A' | 'B' | 'C',
+    syllabus: string
+}
+
+function printCourseInfo(course: courseInfo): void {
+    const courseListEl = document.getElementById("courseList");
+    if (courseListEl) {
+        courseListEl.innerHTML = `${course.name} - ${course.code} - ${course.progression} - ${course.syllabus}`
+    }
+}
+
+const courseForm = document.getElementById("courseForm") as HTMLFormElement;
+
+courseForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nameInput = document.getElementById("name") as HTMLInputElement;
+    const codeInput = document.getElementById("code") as HTMLInputElement;
+    const progressionInput = document.getElementById("progression") as HTMLSelectElement;
+    const urlInput = document.getElementById("url") as HTMLInputElement;
+
+    const newCourse: courseInfo = {
+        code: codeInput.value,
+        name: nameInput.value,
+        progression: progressionInput.value as 'A' | 'B' | 'C',
+        syllabus: urlInput.value
+    }
+
+    printCourseInfo(newCourse);
+});
